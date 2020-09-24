89936886407f1be74d8cae3dfdb6d7b62d8a22ca
Gabriel Marin <gabrielvbmarin21@gmail.com>
2020-09-24 17:08:38 -0300

    adding css and acessing variable in flask

--- app.py
+++ app.py
@@ -15,12 +15,10 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/post_field', methods=["GET", "POST"])
-def post_field():
-    for key, value in request.form.items():
-        print("key: {0}, value: {1}".format(key, value))
-        openBrowser()
-        return render_template("home.html")
+@app.route('/battle/link', methods=["GET", "POST"])
+def battleLink():
+    print(request.form["link"])
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
