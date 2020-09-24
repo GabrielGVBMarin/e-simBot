6dd050f570332ee7e077e95961274b2aa016a3ce
Gabriel Marin <gabrielvbmarin21@gmail.com>
2020-09-24 02:41:42 -0300

    openning browser

--- src/index.py
+++ src/index.py
@@ -1,17 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
-# caps = DesiredCapabilities.FIREFOX.copy()
-# caps['marionette'] = False
-# webdriver.Firefox(capabilities=caps, executable_path='./geckodriver')
-webdriver.Chrome(executable_path='~/Desktop/projects/e-simBot/chromedriver')
-webdriver.get('www.google.com')
+driver = webdriver.Firefox(executable_path='./geckodriver')
 
 
 def main():
     print("SUCCESS")
-    print(sys.path)
 
 
 main()
