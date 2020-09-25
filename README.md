1ff34c4b428b1236f54925e0fda0ee7c32567c81
Gabriel Marin <gabrielvbmarin21@gmail.com>
2020-09-25 00:39:43 -0300

    logging and .env variables

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Need to have firefox installed
+
+Need to set E-SIM-USER and E-SIM-PASSWORD in local variables trough .env
--- src/index.py
+++ src/index.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.select import Select
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 
 
 def openBrowser():
@@ -10,6 +14,24 @@ def getLink(driver, link):
     driver.get(link)
 
 
+def login(driver):
+    driver.find_element_by_xpath("//input[@name='login']").send_keys(os.getenv("E-SIM-USER")
+                                                                     )
+    driver.find_element_by_xpath(
+        "//input[@name='password']").send_keys(os.getenv("E-SIM-PASSWORD")
+                                               )
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+
+
 def run(link):
-    driver = openBrowser()
-    getLink(driver, link)
+    browser = openBrowser()
+    getLink(browser, link)
+    login(browser)
+
+
+def settings():
+    env_path = Path('.') / '../.env'
+    load_dotenv(dotenv_path=env_path)
+
+
+settings()
